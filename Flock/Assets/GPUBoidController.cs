a9581f9723978a4ce9e194bedb09f4c32543d823
kei-kinoshita <keiichiro_kinoshita@brown.edu>
2017-08-22 17:28:54 +0900

    Added trails

--- Flock/Assets/GPUBoidController.cs
+++ Flock/Assets/GPUBoidController.cs
@@ -13,18 +13,21 @@ public class GPUBoidController : MonoBehaviour {
     public float boidRadius;
     public ComputeShader CS;
     public Shader BoidRenderer;
+    public Shader BoidTrailRenderer;
     public GameObject targetPrefab;
     public int blockSize;
 
     private BoidData[] boidsIn;
     private BoidData[] boidsOut;
+    private Trail[] TrailData;
 
     private ComputeBuffer boidBufferRead;
     private ComputeBuffer boidBufferWrite;
     private ComputeBuffer magnetBufferRead;
+    private ComputeBuffer boidTrailBuffer;
 
     private Material BoidMat;
-
+    private Material BoidTrailMat;
 
     private int kernelFlock;
     private int kernelMagnet;
@@ -34,18 +37,33 @@ public class GPUBoidController : MonoBehaviour {
     private List<Magnet> magnets;
     private Camera mainCam;
 
+    private int trailBufferSize;
+    public int numTrails;
+    private int currTrailNum;
 
 	// Use this for initialization
 	void Start () {
         SetUpCS();
         _DrawGizmos = true;
         mainCam = Camera.main;
-        
-	}
+        if (BoidTrailMat == null)
+            BoidTrailMat = new Material(BoidTrailRenderer);
+        BoidTrailMat.SetInt("NUM_TRAILS", numTrails);
+        if (BoidMat == null)
+            BoidMat = new Material(BoidRenderer);
+        currTrailNum = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit mouseHit = new RaycastHit();
+        if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out mouseHit))
+        {
+            CS.SetVector("MOUSE_POS", new Vector3(mouseHit.point.x, mouseHit.point.y, 0) );
+
+        }
+
 
         if (Input.GetMouseButtonDown(0))
         { // Left mouse button clicked
@@ -79,9 +97,12 @@ public class GPUBoidController : MonoBehaviour {
         }
 
         CS.SetFloat("DELTA_TIME", Time.deltaTime);
-        //CS.SetVector("TARGET_1", target1.transform.position);
-        //CS.SetVector("TARGET_2", target2.transform.position);
-        //CS.SetVector("TARGET_3", target3.transform.position);
+
+        currTrailNum = (currTrailNum + 1) % numTrails;
+        //Debug.Log(currTrailNum);
+
+        CS.SetInt("CURR_TRAIL_NUM", currTrailNum);
+        BoidTrailMat.SetInt("CURR_TRAIL_NUM", currTrailNum);
 
         CS.Dispatch(kernelFlock, boidsIn.Length/blockSize, 1, 1);
 
@@ -116,6 +137,20 @@ public class GPUBoidController : MonoBehaviour {
         }
     }
 
+    struct Trail
+    {
+        public Vector3 position;
+        public int trailNum;
+        public int numNeighbors;
+        
+        public Trail(Vector3 pos, int num)
+        {
+            position = pos;
+            trailNum = num;
+            numNeighbors = 0;
+        }
+    }
+
     void SetUpCS()
     {
         boidsIn = new BoidData[numBoids];
@@ -124,8 +159,16 @@ public class GPUBoidController : MonoBehaviour {
         Random.InitState(42);
         for (int i=0; i<numBoids; i++)
         {
-            boidsIn[i].position = new Vector3(Random.value * regionSize, Random.value * regionSize, 0);
-            boidsIn[i].velocity = new Vector3(Random.Range(1f, -1f)*maxSpeed, Random.Range(1f, -1f)*maxSpeed, 0);
+            bool zaxis = false;
+            if (!zaxis)
+            {
+                boidsIn[i].position = new Vector3(Random.value * regionSize, Random.value * regionSize, 0);
+                boidsIn[i].velocity = new Vector3(Random.Range(1f, -1f) * maxSpeed, Random.Range(1f, -1f) * maxSpeed, 0);
+            } else
+            {
+                boidsIn[i].position = new Vector3(Random.value * regionSize, Random.value * regionSize, Random.value * regionSize);
+                boidsIn[i].velocity = new Vector3(Random.Range(1f, -1f) * maxSpeed, Random.Range(1f, -1f) * maxSpeed, Random.Range(1f, -1f) * maxSpeed);
+            }
             boidsIn[i].acceleration = Vector3.zero;
             boidsIn[i].numNeighbors = 0;
         }
@@ -134,17 +177,29 @@ public class GPUBoidController : MonoBehaviour {
         kernelFlock = CS.FindKernel("Flock");
         boidBufferRead = new ComputeBuffer(numBoids, Marshal.SizeOf(typeof(BoidData)));
         boidBufferWrite = new ComputeBuffer(numBoids, Marshal.SizeOf(typeof(BoidData)));
-
-        CS.SetInt("BLOCK_SIZE", blockSize);
         CS.SetBuffer(kernelFlock, "boidBufferRead", boidBufferRead);
         CS.SetBuffer(kernelFlock, "boidBufferWrite", boidBufferWrite);
+
+        // set up trail;
+        trailBufferSize = numTrails * numBoids;
+        boidTrailBuffer = new ComputeBuffer(trailBufferSize, Marshal.SizeOf(typeof(Trail)));
+        TrailData = new Trail[trailBufferSize];
+        for (int i=0; i<trailBufferSize; i++)
+        {
+            TrailData[i] = new Trail(Vector3.zero, (i % numBoids) % numTrails);
+           
+        }
+        boidTrailBuffer.SetData(TrailData);
+        CS.SetBuffer(kernelFlock, "trailBuffer", boidTrailBuffer);
+ 
+        CS.SetInt("BLOCK_SIZE", blockSize);
         CS.SetInt("BUFFER_SIZE",numBoids);
         CS.SetFloat("REGION_SIZE", regionSize);
-
         CS.SetFloat("MAX_SPEED", maxSpeed);
         CS.SetFloat("MAX_FORCE", maxForce);
         CS.SetFloat("NEIGHBOR_DISTANCE", neighborDistance);
         CS.SetFloat("BOID_RADIUS", boidRadius);
+        CS.SetInt("NUM_TRAILS", numTrails);
 
 
         boidBufferRead.SetData(boidsIn);
@@ -167,24 +222,34 @@ public class GPUBoidController : MonoBehaviour {
 
     private void OnRenderObject()
     {
-        if (BoidMat == null)
-            BoidMat = new Material(BoidRenderer);
+
+
+
+        BoidTrailMat.SetBuffer("BoidTrailBuffer", boidTrailBuffer);
+        BoidTrailMat.SetPass(0);
+        Graphics.DrawProcedural(MeshTopology.Points, trailBufferSize);
 
         BoidMat.SetBuffer("BoidBuffer", boidBufferWrite);
         BoidMat.SetPass(0);
-        Graphics.DrawProcedural(MeshTopology.Points, boidsIn.Length);
+        //Graphics.DrawProcedural(MeshTopology.Points, boidsIn.Length);
     }
 
     //private void OnDrawGizmos()
     //{
-    //    if (_DrawGizmos)
+        
+    //    Gizmos.color = Color.white;
+    //    boidTrailBuffer.GetData(TrailData);
+
+
+    //    foreach (Trail t in TrailData)
     //    {
-    //        Gizmos.color = Color.white;
-    //        foreach (BoidData b in boidsIn)
-    //        {
-    //            Gizmos.DrawLine(b.position, b.position + b.velocity);
-    //        }
+
+
+
+    //        Gizmos.DrawLine(t.position, t.position + Vector3.right*10f);
+
     //    }
+        
     //}
 
 
@@ -198,6 +263,7 @@ public class GPUBoidController : MonoBehaviour {
     {
         boidBufferRead.Release();
         boidBufferWrite.Release();
+        boidTrailBuffer.Release();
     }
 
     void SwapBuffer(ref ComputeBuffer src, ref ComputeBuffer dst)
---
+++ Flock/Assets/GPUBoidTrailRenderer.shader
@@ -0,0 +1,80 @@
+﻿Shader "SPATIALFLOW/BoidTrailRenderer" 
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+	CGINCLUDE		
+	#include "UnityCG.cginc"
+
+	struct v2f
+	{
+		float4 pos : POSITION;
+		int trailID : TEXCOORD0;
+		int numNeighbors : TEXCOORD1;
+	};
+
+
+	struct Trail
+	{
+		float3 position;
+		int trailNum;
+		int numNeighbors;
+	};
+
+	sampler2D _MainTex;
+	float4 _MainTex_ST;
+	int CURR_TRAIL_NUM;
+	int NUM_TRAILS;
+
+	StructuredBuffer<Trail> BoidTrailBuffer;
+			
+	v2f vert (uint id : SV_VertexID)
+	{
+		v2f o;
+
+		float4 _pos = float4(BoidTrailBuffer[id].position, 1);
+		o.pos = UnityObjectToClipPos(_pos);
+		o.trailID = BoidTrailBuffer[id].trailNum;
+		o.numNeighbors =BoidTrailBuffer[id].numNeighbors;
+		return o;
+	}
+			
+	float4 frag (v2f i) : SV_Target
+	{
+		float trailLerp = ((float)(NUM_TRAILS-(   (i.trailID+ NUM_TRAILS-CURR_TRAIL_NUM)%NUM_TRAILS)      ))/(float)NUM_TRAILS;
+		
+
+		float4 blue = float4(1,1,1,0)*(1-trailLerp) + float4(0,0.3,1,0)*(trailLerp);
+		float4 red = float4(1,0,1,0)*(1-trailLerp) + float4(0,0,0.7,0)*(trailLerp);
+		//float4 color = red*smoothstep(0,50,i.numNeighbors) + blue*smoothstep(50,0,i.numNeighbors);
+		float4 color = blue;
+		color.a = (1-trailLerp);
+		return color;
+		//		Tags { "RenderType"="Transparent" "Queue" = "Geometry"}
+
+	}
+	ENDCG
+
+	SubShader
+	{
+	    Tags {"Queue"="Transparent" "RenderType"="Transparent" "LightMode"="ForwardBase"}
+
+		LOD 100
+
+		Pass
+		{
+
+		    
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			ENDCG
+		}
+	}
+	
+	
+}
